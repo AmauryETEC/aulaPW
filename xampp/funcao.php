73debce3e10be9b4a946f935642a523810b5db0c
amauryETEC <zafani19@gmail.com>
2022-04-13 07:52:45 -0300

    https://github.com/AmauryETEC/aulaPW.git

---
+++ xampp/funcao.php
@@ -0,0 +1,12 @@
+<?php
+    $sorteio = rand(10,55),//rand-função faz o sorteio
+    echo "o número é $sorteio";
+
+    if{
+        ($numero %2 == 0);
+        echo "o número é par"
+    }
+    else{
+        echo "o número é impar"
+    }
+?>
