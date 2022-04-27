8e284bcbee5e3e6ce619fbe9367af3bf51158002
AmauryETEC <92283602+AmauryETEC@users.noreply.github.com>
2022-04-27 07:57:24 -0300

    Add files via upload

---
+++ atividade/substituicao.php
@@ -0,0 +1,12 @@
+<?php
+
+    $texto = "Eu adoro PHP";
+
+    //str_replace substitui partes de uma string 
+
+    $troca = str_replace("adoro", "amo", $texto);
+
+    echo $texto;
+    echo "</br></br>";
+    echo $troca;
+?>
