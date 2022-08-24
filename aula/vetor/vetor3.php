48b257e1ecd49c2e642743cfb6efdc5a541e8432
AmauryETEC <92283602+AmauryETEC@users.noreply.github.com>
2022-08-24 12:14:58 -0300

    Add files via upload

---
+++ aula/vetor/vetor3.php
@@ -0,0 +1,19 @@
+<?php
+
+    $cores = array(
+
+        "Azul",
+        "Amarelo",
+        "Rosa",
+        "Azul",
+        "Amarelo",
+        "Rosa",
+        "Azul",
+        "Amarelo",
+        "Rosa"
+    );
+
+    echo var_dump($cores);
+
+
+?>
---
+++ aula/vetor/vetor5.php
@@ -0,0 +1,20 @@
+<?php
+
+    $vetor = array (
+
+        "Amaury",
+        10.6,
+        "pastel de flango",
+        4,
+        10,
+        12,
+        "Rua A"
+
+    );
+
+    echo var_dump($vetor);
+    // count() conta a quantidade de posições
+    echo "</br> A quantia de posições é:" . count($vetor);
+
+
+?>
