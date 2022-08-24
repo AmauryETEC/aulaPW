b1bb2cf916c144a6bb2461d2403bc3800aa40882
AmauryETEC <92283602+AmauryETEC@users.noreply.github.com>
2022-08-24 07:43:06 -0300

    Add files via upload

---
+++ aula/17.08/funcao3.php
@@ -0,0 +1,24 @@
+<?php
+
+    function verificar($v1, $v2){
+        if($v1 > $v2){
+            while($v1 >= $v2){
+                if($v1 % 2 == 1){
+                    echo $v1 . "</br>";
+                }
+            $v1--;
+            }
+        }
+        else{
+            while($v2 >= $v1){
+                if($v2 % 2 == 1){
+                    echo $v2 . "</br>";
+                }
+                $v2--;
+            }
+        }
+    }
+
+        echo verificar(10, 52);
+
+?>
